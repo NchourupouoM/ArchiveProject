32b2019a064a9ed8375517f58eb4f545d49b0c77
NchourupouoM <nchourupouom04@gmail.com>
2024-09-27 21:16:38 +0100

    mise a jour de l'application authentication

--- MekinArchiving/authentication/urls.py
+++ MekinArchiving/authentication/urls.py
@@ -1,16 +1,8 @@
-from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
+from django.urls import path
 
-from django.contrib.auth.views import LoginView, LogoutView
-import authentication
 import authentication.views
 
 urlpatterns = [
-    path('',LoginView.as_view(
-        template_name='authentication/login.html',
-        redirect_authenticated_user =True),
-        name="login"),
+    path('',authentication.views.LoginPageView.as_view(), name = 'login'),
     path('logout/',authentication.views.logout_user, name='logout'),
 ]
--- MekinArchiving/authentication/views.py
+++ MekinArchiving/authentication/views.py
@@ -1,7 +1,30 @@
-from django.shortcuts import render
+from django.views.generic import View
+from authentication import forms
 from django.shortcuts import render,redirect
-from django.contrib.auth import logout
-# Create your views here.
+from django.contrib.auth import logout, authenticate, login
+
+class LoginPageView(View):
+    template_name = 'authentication/login.html'
+    form_class = forms.LoginForm
+    
+    def get(self,request):
+        form = self.form_class()
+        message = ''
+        return render(request,self.template_name,context = {'form':form,'message':message})
+    
+    def post(self,request):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            user = authenticate(
+                username=form.cleaned_data["username"],
+                password=form.cleaned_data["password"],
+            )
+            if user is not None :
+                login(request,user)
+                return redirect('home')
+        message = "Identifiants invalide."
+        return render(request,self.template_name,context = {'form':form,'message':message})
+
 def logout_user(request):
     logout(request)
     return redirect('login')
