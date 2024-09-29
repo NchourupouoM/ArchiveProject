d2160d7fb5f334a427fd9655a06eff7a55cdf1b4
NchourupouoM <nchourupouom04@gmail.com>
2024-09-29 10:38:11 +0100

    Ajout de la fonctionnalite de recherche par similarite

--- MekinArchiving/search/views.py
+++ MekinArchiving/search/views.py
@@ -1,6 +1,6 @@
 from search.forms import SearchForm
 from archives.models import Archive
-from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 from django.shortcuts import render
 
 def searchDocument(request):
@@ -12,8 +12,12 @@ def searchDocument(request):
         search_form = SearchForm(request.GET)
         if search_form.is_valid():
             query = search_form.cleaned_data['query']
+
+            vector_search = SearchVector('titre','metadonnees')
+            query_search = SearchQuery(query)
+
             results = Archive.published.annotate(
-                search = SearchVector('titre','metadonnees'),
-            ).filter(search=query)
+                search = vector_search ,rank =SearchRank(vector_search, query_search)
+            ).filter(search=query_search).order_by('-rank')
 
     return render(request=request, template_name="search/search_result.html",context={"search_form":search_form,'query':query, 'results':results})
