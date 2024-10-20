36b91ad29bb12bba58626b6fb04b5f6540960f77
NchourupouoM <nchourupouom04@gmail.com>
2024-10-20 12:13:16 +0100

    design de la page de modification de la photo de profil

--- MekinArchiving/search/models.py
+++ MekinArchiving/search/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from datetime import datetime
+from django.conf import settings
+
 
 # Create your models here.
+class Room(models.Model):
+    name = models.CharField(max_length=1000000)
+
+class Message(models.Model):
+    value = models.CharField(max_length=1000000)
+    date = models.DateTimeField(default=datetime.now,blank=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.DO_NOTHING)
+    room = models.ForeignKey(Room, related_name='message_room', on_delete=models.CASCADE)   
